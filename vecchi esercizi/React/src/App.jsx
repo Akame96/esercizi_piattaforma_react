b5e05a253f2d13a264e887cf0cc775fd6d77ec88
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-23 17:57:27 +0200

    nuovi esercizi react

--- vecchi esercizi/React/src/App.jsx
+++ vecchi esercizi/React/src/App.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./Welcome";
-import Counter from "./Counter";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './Welcome';
+import Counter from './Counter';
+import ShowGithubUser from './ShowGithubUser';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome name="Nunzia" />} />
+        <Route path="/" element={<Welcome name="John Doe" />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
+
