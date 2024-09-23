467ee05d49ba8f5b8b2006c6a75b1f87c9371c2c
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-23 18:11:20 +0200

    esercizi react aggiornati

--- vecchi esercizi/React/src/App.jsx
+++ vecchi esercizi/React/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Welcome from './Welcome';
 import Counter from './Counter';
 import ShowGithubUser from './ShowGithubUser';
@@ -6,14 +6,30 @@ import ShowGithubUser from './ShowGithubUser';
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome name="John Doe" />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/users/:username" element={<ShowGithubUser />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+            <li>
+              <Link to="/users/Akame">GitHub User:Akame </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Welcome name="Nunzia" />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users/:username" element={<ShowGithubUser />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
 
 export default App;
+
 
